d63c7581b7187ede4baf0e115998eed7dd117a86
Ana Maria Chaleunrath <anamaria@Anas-Air.att.net>
2017-01-09 11:10:54 -0800

    redesign

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 
     $('.dropdown-content li').on('click', function() {
-        $('.app-description').remove(); //clear app description
+        // $('.app-description').remove(); //clear app description
         $('.dropbtn').text($(this).text());
         var input = $(this).text();
         callAPI(input); //get user input and send it to the API function
@@ -32,16 +32,20 @@ function showBookInfo(topFive) {
     $.each(topFive, function(index, book) {
 
         //add results of API call to the page
-        $('#results, tr').append('<th class=table-heading><img id="' + book.rank + '"src=' + book.book_image + '>' + '<div class="text-box-style"><p class="hide-txt">' + "Rank: " + book.rank + '</p>' + '<p class="author hide-txt">' + "Author: " + book.author + '</p>' + '<p class="description hide-txt">' + book.description + '</p>' + '<a class="amazon-link" target =_blank href=' + book.amazon_product_url + '>Buy it</a>' + '</div></th>');
+        $('#results, tr').append('<th class=table-heading><img id="' + book.rank + '"src=' + book.book_image + '>' + 
+            '<div class="text-box-style"><p class="hide-txt">' + "Rank: " + book.rank + '</p>' + 
+            '<p class="author hide-txt">' + "Author: " + book.author + '</p>' + '<p class="description hide-txt">' + 
+            book.description + '</p>' + '</div></th>');
+        $('#' + book.rank).wrap('<a target=_blank href="' + book.amazon_product_url + '" />');
         //hide and unhide book info on image hover
-        $('#' + book.rank).mouseenter(function() {
-            $(this).parent().find('p').removeClass('hide-txt');
-            $(this).parent().find('a').addClass('hide-txt');
+        // $('#' + book.rank).mouseenter(function() {
+        //     $(this).parent().find('p').removeClass('hide-txt');
+        //     $(this).parent().find('a').addClass('hide-txt');
 
-        }).mouseleave(function() {
-            $(this).parent().find('p').addClass('hide-txt');
-            $(this).parent().find('a').removeClass('hide-txt');
-        });
+        // }).mouseleave(function() {
+        //     $(this).parent().find('p').addClass('hide-txt');
+        //     $(this).parent().find('a').removeClass('hide-txt');
+        // });
     });
 }
 
